cf7b5d47b9b6872e1af822a30377f86eb831c7d0
mariam-hanna <mariam.hanna.2013@gmail.com>
2014-06-14 00:11:54 +0300

    25r_7aga_isa:)

---
+++ application/controllers/EvaluationsController.php
@@ -0,0 +1,41 @@
+<?php
+
+class EvaluationsController extends Zend_Controller_Action
+{
+
+    private $sess = null;
+
+    private $user_data = null;
+
+    public function init()
+    {
+        $cats = new Application_Model_Category();
+        $this->view->cats = $cats->listAllCategory();
+        $this->user_data = Zend_Auth::getInstance()->getStorage()->read();
+        $this->sess = new Zend_Session_Namespace("Zend_Auth");
+        $authorization = Zend_Auth::getInstance();
+        $action = $this->getRequest()->getActionName();
+        $_SESSION['action'] = $action;
+        if ($this->user_data->admin == 'false') {
+            $this->redirect("/user/");
+        } else if (!$authorization->hasIdentity()) {
+            $this->redirect("/user/login");
+        }
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function storiesAction()
+    {
+        $stories = new Application_Model_Story();
+        $this->view->stories = $stories->getMostWatchedStories();
+    }
+
+
+}
+
+
+
